d54189cae02e1939eb8851da8b4015577e18e2d3
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2024-10-26 17:52:13 +0200

    nullable option array

--- FartingUnicorn.Tests/SingleField_NonNullableNonOptional_Tests.cs
+++ FartingUnicorn.Tests/SingleField_NonNullableNonOptional_Tests.cs
@@ -1467,19 +1467,19 @@ public class Objects
             public void MissingFields()
             {
                 var json = JsonSerializer.Deserialize<JsonElement>("""
-            {
-                "Comments": [
                     {
-                        "Upvotes": 5,
-                        "Contact": "John Doe"
-                    },
-                    {
-                        "Text": "Second!",
-                        "Upvotes": 3
+                        "Comments": [
+                            {
+                                "Upvotes": 5,
+                                "Contact": "John Doe"
+                            },
+                            {
+                                "Text": "Second!",
+                                "Upvotes": 3
+                            }
+                        ]
                     }
-                ]
-            }
-            """);
+                    """);
                 var blogPost = Mapper.Map<BlogPost>(json);
                 blogPost.Success.Should().BeFalse();
                 blogPost.Errors.Should().ContainSingle(e => e.Message == "Comments.0.Text is required");
@@ -1598,6 +1598,153 @@ public class Objects
 
             }
 
+        }
+    
+        public class Nullable
+        {
+            public class BlogPost
+            {
+                public Comment[]? Comments { get; set; }
+            }
+
+            public class Comment
+            {
+                public string Text { get; set; }
+                public int Upvotes { get; set; }
+                public Option<string> Contact { get; set; }
+            }
+
+            [Fact]
+            public void Valid()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+            {
+                "Comments": [
+                    {
+                        "Text": "First!",
+                        "Upvotes": 5,
+                        "Contact": "John Doe"
+                    },
+                    {
+                        "Text": "Second!",
+                        "Upvotes": 3,
+                        "Contact": null
+                    }
+                ]
+            }
+            """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeTrue();
+                blogPost.Value.Comments.Should().HaveCount(2);
+                blogPost.Value.Comments[0].Text.Should().Be("First!");
+                blogPost.Value.Comments[0].Upvotes.Should().Be(5);
+                blogPost.Value.Comments[0].Contact.Should().BeOfType<Some<string>>();
+                var someAuthor = (blogPost.Value.Comments[0].Contact as Some<string>)!;
+                someAuthor.Value.Should().Be("John Doe");
+                blogPost.Value.Comments[1].Text.Should().Be("Second!");
+                blogPost.Value.Comments[1].Upvotes.Should().Be(3);
+                blogPost.Value.Comments[1].Contact.Should().BeOfType<None<string>>();
+            }
+
+            [Fact]
+            public void MissingArray_OK()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                    }
+                    """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeTrue();
+                blogPost.Value.Comments.Should().BeNull();
+            }
+
+            [Fact]
+            public void NulledArray_NotOk()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                        "Comments": null
+                    }
+                    """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeFalse();
+                blogPost.Errors.Should().ContainSingle(e => e.Message == "Comments must have a value");
+            }
+        }
+    
+        public class NullableOptional
+        {
+            public class BlogPost
+            {
+                public Option<Comment[]>? Comments { get; set; }
+            }
+
+            public class Comment
+            {
+                public string Text { get; set; }
+                public int Upvotes { get; set; }
+                public Option<string> Contact { get; set; }
+            }
+
+            [Fact]
+            public void Valid()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+            {
+                "Comments": [
+                    {
+                        "Text": "First!",
+                        "Upvotes": 5,
+                        "Contact": "John Doe"
+                    },
+                    {
+                        "Text": "Second!",
+                        "Upvotes": 3,
+                        "Contact": null
+                    }
+                ]
+            }
+            """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeTrue();
+                blogPost.Value.Comments.Should().BeOfType<Some<Comment[]>>();
+                var someComments = (blogPost.Value.Comments as Some<Comment[]>)!;
+                someComments.Value.Should().HaveCount(2);
+                someComments.Value[0].Text.Should().Be("First!");
+                someComments.Value[0].Upvotes.Should().Be(5);
+                someComments.Value[0].Contact.Should().BeOfType<Some<string>>();
+                var someAuthor = (someComments.Value[0].Contact as Some<string>)!;
+                someAuthor.Value.Should().Be("John Doe");
+                someComments.Value[1].Text.Should().Be("Second!");
+                someComments.Value[1].Upvotes.Should().Be(3);
+                someComments.Value[1].Contact.Should().BeOfType<None<string>>();
+                var someAuthor2 = (someComments.Value[1].Contact as None<string>)!;
+            }
+
+            [Fact]
+            public void MissingArray_OK()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                    }
+                    """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeTrue();
+                blogPost.Value.Comments.Should().BeNull();
+            }
+
+            [Fact]
+            public void NulledArray_Ok()
+            {
+                var json = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                        "Comments": null
+                    }
+                    """);
+                var blogPost = Mapper.Map<BlogPost>(json);
+                blogPost.Success.Should().BeTrue();
+                blogPost.Value.Comments.Should().BeOfType<None<Comment[]>>();
+            }
         }
     }
 }
