885e01df845890dda98331ab11cc886ec248c6dd
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2024-11-10 22:43:37 +0100

    .

---
+++ FartingUnicorn.Tests/Generated/MapperGenerator/MapperGenerator.MapperGenerator/Mapper.FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.g.cs
@@ -0,0 +1,71 @@
+﻿using DotNetThoughts.Results;
+using System.Text.Json;
+using static FartingUnicorn.MapperOptions;
+
+namespace FartingUnicorn.Generated;
+
+public static partial class Mappers
+{
+    public static Result<FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost> MapToFartingUnicorn_Tests_SingleField_EnumType_NullableOptional_Tests_BlogPost(JsonElement jsonElement, MapperOptions mapperOptions = null, string[] path = null)
+    {
+        if (mapperOptions is null)
+        {
+            mapperOptions = new MapperOptions();
+        }
+        if (path is null)
+        {
+            path = ["$"];
+        }
+        if (jsonElement.ValueKind != JsonValueKind.Object)
+        {
+            return Result<FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost>.Error(new ValueHasWrongTypeError(path, "Object", jsonElement.ValueKind.ToString()));
+        }
+        var obj = new FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost();
+
+        List<IError> errors = new();
+        var isStatusPropertyDefined = jsonElement.TryGetProperty("Status", out var jsonStatusProperty);
+        if (isStatusPropertyDefined)
+        {
+            // type = FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.BlogPostStatus, isOption = True, isNullable = True
+            if (jsonStatusProperty.ValueKind == JsonValueKind.Null)
+            {
+                obj.Status = new None<FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.BlogPostStatus>();
+            }
+            else if (mapperOptions.TryGetConverter(typeof(FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.BlogPostStatus), out IConverter customConverter))
+            {
+                if (jsonStatusProperty.ValueKind != customConverter.ExpectedJsonValueKind)
+                {
+                    errors.Add(new ValueHasWrongTypeError([.. path, "Status"], customConverter.ExpectedJsonValueKind.ToString(), jsonStatusProperty.ValueKind.ToString()));
+                }
+                else
+                {
+                    var result = customConverter.Convert(typeof(FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.BlogPostStatus), jsonStatusProperty, mapperOptions, [.. path, "Status"]);
+                    if (result.Success)
+                    {
+                        obj.Status = result.Map(x => (FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost.BlogPostStatus)x).Value;
+                    }
+                    else
+                    {
+                        errors.AddRange(result.Errors.Select(x => new MappingError([.. path, "{p.Name}"], x.Message)).ToArray());
+                    }
+                }
+            }
+        }
+        else
+        {
+            obj.Status = null;
+        }
+        if(errors.Any())
+        {
+            return Result<FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost>.Error(errors);
+        }
+        if(false)/*check if is option*/
+        {
+        }
+        else
+        {
+            return Result<FartingUnicorn.Tests.SingleField.EnumType.NullableOptional_Tests.BlogPost>.Ok(obj);
+        }
+        throw new NotImplementedException();
+    }
+}
--- FartingUnicorn.Tests/SingleField.cs
+++ FartingUnicorn.Tests/SingleField.cs
@@ -1055,135 +1055,152 @@ public class SingleField
             }
         }
 
-        //public class NonNullableOptional_Tests
-        //{
-        //    public static IEnumerable<object[]> GetMappers =>
-        //    [
-        //        [(Func<JsonElement, Result<BlogPost>>)(x => Map<BlogPost>(x, null, null))],
-        //        [(Func<JsonElement, Result<BlogPost>>)(x => FartingUnicorn.Generated.Mappers.MapToFartingUnicorn_Tests_SingleField_IntType_NonNullableOptional_Tests_BlogPost(x, null))]
-        //    ];
-        //    [CreateMapper]
-        //    public class BlogPost
-        //    {
-        //        /// <summary>
-        //        /// Field must exist
-        //        /// Value can be null
-        //        /// </summary>
-        //        public Option<int> Rating { get; set; }
-        //    }
+        public class NonNullableOptional_Tests
+        {
+            public static IEnumerable<object[]> GetMappers
+            {
+                get
+                {
+                    var mapperOptions = new MapperOptions();
+                    mapperOptions.AddConverter(new EnumAsStringConverter());
+                    return [
+                        [(Func<JsonElement, Result<BlogPost>>)(x => Map<BlogPost>(x, mapperOptions, null))],
+                        [(Func<JsonElement, Result<BlogPost>>)(x => Generated.Mappers.MapToFartingUnicorn_Tests_SingleField_EnumType_NonNullableOptional_Tests_BlogPost(x, mapperOptions, null))]
+                    ];
+                }
+            }
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void ValidSingleField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //              "Rating": 5
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+            [CreateMapper]
+            public class BlogPost
+            {
+                public enum BlogPostStatus { Draft, Published }
+                /// <summary>
+                /// Field must exist
+                /// Value can be null
+                /// </summary>
+                public Option<BlogPostStatus> Status { get; set; }
+            }
 
-        //        Assert.True(blogPost.Success);
-        //        blogPost.Value.Rating.Should().BeOfType<Some<int>>();
-        //        var someIsDraft = (blogPost.Value.Rating as Some<int>)!;
-        //        someIsDraft.Value.Should().Be(5);
-        //    }
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void ValidSingleField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                      "Status": "Published"
+                    }
+                    """);
+                var blogPost = map(jsonElement);
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void MissingNonNullableField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+                Assert.True(blogPost.Success);
+                blogPost.Value.Status.Should().BeOfType<Some<BlogPost.BlogPostStatus>>();
+                var someIsDraft = (blogPost.Value.Status as Some<BlogPost.BlogPostStatus>)!;
+                someIsDraft.Value.Should().Be(BlogPost.BlogPostStatus.Published);
+            }
 
-        //        Assert.False(blogPost.Success);
-        //        blogPost.Errors.Should().ContainSingle();
-        //        blogPost.Errors.Single().Should().BeOfType<RequiredPropertyMissingError>();
-        //        blogPost.Errors.Single().Message.Should().Be("$.Rating is required");
-        //    }
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void MissingNonNullableField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                    }
+                    """);
+                var blogPost = map(jsonElement);
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void NulledOptionalField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //              "Rating": null
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+                Assert.False(blogPost.Success);
+                blogPost.Errors.Should().ContainSingle();
+                blogPost.Errors.Single().Should().BeOfType<RequiredPropertyMissingError>();
+                blogPost.Errors.Single().Message.Should().Be("$.Status is required");
+            }
 
-        //        Assert.True(blogPost.Success);
-        //        blogPost.Value.Rating.Should().BeOfType<None<int>>();
-        //    }
-        //}
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void NulledOptionalField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                      "Status": null
+                    }
+                    """);
+                var blogPost = map(jsonElement);
 
-        //public class NullableOptional_Tests
-        //{
-        //    public static IEnumerable<object[]> GetMappers =>
-        //    [
-        //        [(Func<JsonElement, Result<BlogPost>>)(x => Map<BlogPost>(x, null, null))],
-        //        [(Func<JsonElement, Result<BlogPost>>)(x => FartingUnicorn.Generated.Mappers.MapToFartingUnicorn_Tests_SingleField_IntType_NullableOptional_Tests_BlogPost(x, null))]
-        //    ];
+                Assert.True(blogPost.Success);
+                blogPost.Value.Status.Should().BeOfType<None<BlogPost.BlogPostStatus>>();
+            }
+        }
 
-        //    [CreateMapper]
-        //    public class BlogPost
-        //    {
-        //        /// <summary>
-        //        /// Field can be missing
-        //        /// Value can be null
-        //        /// </summary>
-        //        public Option<int>? Rating { get; set; }
-        //    }
+        public class NullableOptional_Tests
+        {
+            public static IEnumerable<object[]> GetMappers
+            {
+                get
+                {
+                    var mapperOptions = new MapperOptions();
+                    mapperOptions.AddConverter(new EnumAsStringConverter());
+                    return [
+                        [(Func<JsonElement, Result<BlogPost>>)(x => Map<BlogPost>(x, mapperOptions, null))],
+                        [(Func<JsonElement, Result<BlogPost>>)(x => Generated.Mappers.MapToFartingUnicorn_Tests_SingleField_EnumType_NullableOptional_Tests_BlogPost(x, mapperOptions, null))]
+                    ];
+                }
+            }
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void ValidSingleField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //              "Rating": 5
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+            [CreateMapper]
+            public class BlogPost
+            {
+                public enum BlogPostStatus { Draft, Published }
+                /// <summary>
+                /// Field can be missing
+                /// Value can be null
+                /// </summary>
+                public Option<BlogPostStatus>? Status { get; set; }
+            }
 
-        //        blogPost.Should().BeSuccessful();
-        //        blogPost.Value.Rating.Should().BeOfType<Some<int>>();
-        //        var someIsDraft = (blogPost.Value.Rating as Some<int>)!;
-        //        someIsDraft.Value.Should().Be(5);
-        //    }
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void ValidSingleField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                      "Status": "Published"
+                    }
+                    """);
+                var blogPost = map(jsonElement);
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void MissingOptionalField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+                blogPost.Should().BeSuccessful();
+                blogPost.Value.Status.Should().BeOfType<Some<BlogPost.BlogPostStatus>>();
+                var someIsDraft = (blogPost.Value.Status as Some<BlogPost.BlogPostStatus>)!;
+                someIsDraft.Value.Should().Be(BlogPost.BlogPostStatus.Published);
+            }
 
-        //        blogPost.Should().BeSuccessful();
-        //        blogPost.Value.Rating.Should().BeNull();
-        //    }
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void MissingOptionalField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                    }
+                    """);
+                var blogPost = map(jsonElement);
 
-        //    [Theory]
-        //    [MemberData(nameof(GetMappers))]
-        //    public void NulledOptionalField(Func<JsonElement, Result<BlogPost>> map)
-        //    {
-        //        var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
-        //            {
-        //              "Rating": null
-        //            }
-        //            """);
-        //        var blogPost = map(jsonElement);
+                blogPost.Should().BeSuccessful();
+                blogPost.Value.Status.Should().BeNull();
+            }
 
-        //        blogPost.Should().BeSuccessful();
-        //        blogPost.Value.Rating.Should().BeOfType<None<int>>();
-        //    }
-        //}
+            [Theory]
+            [MemberData(nameof(GetMappers))]
+            public void NulledOptionalField(Func<JsonElement, Result<BlogPost>> map)
+            {
+                var jsonElement = JsonSerializer.Deserialize<JsonElement>("""
+                    {
+                      "Status": null
+                    }
+                    """);
+                var blogPost = map(jsonElement);
+
+                blogPost.Should().BeSuccessful();
+                blogPost.Value.Status.Should().BeOfType<None<BlogPost.BlogPostStatus>>();
+            }
+        }
     }
 }
